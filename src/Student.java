3f3f789fb5f48172897a631e5941a7ad1a104ccd
Админ <241656@astanait.edu.kz>
2025-04-27 18:14:01 +0500

    init commit

---
+++ src/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    private int id;
+    private String name;
+
+    // Constructor
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Getter methods
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
